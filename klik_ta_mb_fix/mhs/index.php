249beb1be9e8e4f82616436852b30fc573cf8df1
rumahrudi <rudy2001hariadi@gmail.com>
2024-06-09 18:15:50 +0700

    update
    change name of sita if to mb

--- klik_ta_mb_fix/mhs/index.php
+++ klik_ta_mb_fix/mhs/index.php
@@ -96,7 +96,7 @@ $nama = $_SESSION['nama'];
     <div class="login-logo">
       <!--<img src="img/logo2.png" height="60%" width="180" class="rounded" alt="..."><br>-->
       <a href="index.php?page=home"><b>SI</b>TA</a>
-      <h5>Sistem Informasi Tugas Akhir Teknik Informatika <?php echo $nama; ?></h5>
+      <h5>Sistem Informasi Tugas Akhir Manajemen Bisnis <?php echo $nama; ?></h5>
     </div>
     <!-- /.login-logo -->
     <div class="login-box-body">
